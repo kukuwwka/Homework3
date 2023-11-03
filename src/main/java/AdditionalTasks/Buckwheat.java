f9076e5cee677f4ab24e6bb2a35f6ef58f9a0689
kukuwwka <ququwka1@gmail.com>
2023-11-03 20:41:40 +0400

    Initial commit

---
+++ src/main/java/AdditionalTasks/Buckwheat.java
@@ -0,0 +1,2 @@
+package AdditionalTasks;public class Buckwheat {
+}
---
+++ src/main/java/MainTasks/FizzBuzz.java
@@ -0,0 +1,16 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i + " fizz");
+            }
+            if (i % 5 == 0) {
+                System.out.println(i + " buzz");
+            }
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i + " FizzBuzz");
+            }
+        }
+    }
+}
