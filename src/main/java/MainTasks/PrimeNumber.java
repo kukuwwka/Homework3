89f731385dfdb322ca97e0bc06dc34822819d2c8
kukuwwka <ququwka1@gmail.com>
2023-11-05 00:00:44 +0400

    initial commit

--- src/main/java/MainTasks/PrimeNumber.java
+++ src/main/java/MainTasks/PrimeNumber.java
@@ -1,13 +1,26 @@
 package MainTasks;
 
-import java.math.BigInteger;
+
+import java.util.Scanner;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        for (Integer a = 2; a < 100; a++) {
-            BigInteger bigInteger = BigInteger.valueOf(a);
-            boolean probablePrime = bigInteger.isProbablePrime((int) Math.log(a));
-            System.out.println(a + " число простое? - " + probablePrime);
+        int value;
+        boolean isPrime = true;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        int number = scanner.nextInt();
+        for (int a = 2; a <= number / 2; a++) {
+            value = number % a;
+            if (value == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.println(number + " простое число");
+        } else {
+            System.out.println(number + " составное число");
         }
     }
 }
